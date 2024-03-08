659ab7625d37c375e12b3d1d8ffe57ed802ac44e
AnnaT7459 <141446349+AnnaT7459@users.noreply.github.com>
2024-03-07 20:50:53 -0600

    added logic to connect back end to front end add trip component

--- client/src/components/AddTripComponent.jsx
+++ client/src/components/AddTripComponent.jsx
@@ -1,13 +1,47 @@
 import randomImg from "../utils/randomImg";
+import { useState } from 'react';
+import { CREATE_TRIP } from '../utils/mutations';
+import { useMutation } from 'apollo/client';
 
 const AddTripComponent = () => {
+    const [tripData, setTripData] = useState({
+        where: '',
+        departureDate: '',
+        returnDate: '',
+        budget: 0,
+        description: '',
+    });
+
+    const [createTrip, { error }] = useMutation(CREATE_TRIP);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setTripData({...tripData,[name]: value,});
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const { data } = await createTrip({ variable: { tripInput: tripData },
+            });
+            // successful
+            console.log('Trip created!', data.createTrip);
+    } catch (err) {
+        console.error('Error creating trip!, err');
+    };
+
     return (             
     <section>
-        <form action="/add-trip" style={{ backgroundImage: `url(${randomImg()})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }} className="addTrip">
+        <form onSubmit={handleSubmit} style={{ backgroundImage: `url(${randomImg()})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }} className="addTrip">
 
-            <input id="whereInput" className='title color-change' type="text" 
+            <input 
+            id="whereInput" 
+            className='title color-change' 
+            type="text" 
             name="where" 
             placeholder="Where to?" 
+            value={tripData.where}
+            onChange={handleChange}
             /><br />
 
             {/* input for IMG upload */}
@@ -19,18 +53,35 @@ const AddTripComponent = () => {
 
             <div className="dateBudget-container">
                 <label className="title departure-date">Departure Date:</label><br />
-
-                <input type="date" className="color-change dateInput" 
+                {/* input DEPARTURE date */}
+                <input 
+                id="depDateInput" 
+                className="color-change dateInput" 
+                type="date"
+                name="depart"
+                value={tripData.depart}
+                onChange={handleChange}
                 /><br />
         
                 {/* input RETURN Date */}
-                <input type="date" className="color-change dateInput" 
+                <input 
+                id="retDateInput" 
+                className="color-change dateInput" 
+                type="date"
+                name="return"
+                value={tripData.return}
+                onChange={handleChange}
                 /><br />
             
                 {/* input BUDGET */}
                 <label className="title budget-title"> Budget: </label> <br />
-                <span>$ </span><input type="number" className="color-change budgetInput" 
-                placeholder="10,000"
+                <span>$ </span><input 
+                id="budgetInput" 
+                className="color-change budgetInput" 
+                type="number" 
+                name="budget"
+                value={tripData.budget}
+                onChange={handleChange}
                 /><span> $</span> <br />
                 <label>(USD)</label><br />
             </div>
@@ -38,8 +89,14 @@ const AddTripComponent = () => {
             <label className="title description-title">Describe what you would like to do on this trip!!</label><br />
 
             {/* input DESCRIPTION addTrip */}
-            <textarea type="date" className="color-change textAreaInput" 
+            <textarea 
+            id="descriptionInput"
+            className="color-change textAreaInput" 
+            type="text"
+            name="description"
             placeholder="We're Excited to hear your plan!!"
+            value={tripData.budget}
+            onChange={handleChange}
             /> <br />
 
             <button type="submit" className="addTrip-submit ">Add Trip</button>
