2a26f5f301d4914d04244707009da1390ced5e9d
Harmony Burke <theharmonyburke@gmail.com>
2024-03-07 19:40:16 -0600

    created FE queries

---
+++ client/utils/queries.js
@@ -0,0 +1,58 @@
+import { gql } from "@apollo/client";
+
+export const QUERY_TRIPS = gql`
+  query allTrips {
+    trips {
+      _id
+      budget
+      where
+      description
+      user
+      img
+      departureDate
+      returnDate
+      createdAt
+      joined
+    }
+  }
+`;
+
+export const QUERY_SINGLE_TRIP = gql`
+  query signleTrip($tripId: ID!) {
+    trip(tripId: $tripId) {
+      _id
+      budget
+      where
+      description
+      user
+      img
+      departureDate
+      returnDate
+      createdAt
+      joined
+    }
+  }
+`;
+
+export const QUERY_USERS = gql`
+  query allUsers {
+    users {
+      _id
+      name
+      email
+      password
+      trips
+    }
+  }
+`;
+
+export const QUERY_SINGLE_USER= gql`
+query singleUser($usedId:ID!){
+    user(userId: $userId){
+        _id
+        name
+        email
+        password
+        trips
+    }
+}`;
