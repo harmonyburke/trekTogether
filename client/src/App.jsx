7530e700e2ab54863b50a1d18bbcb74e2db46bb6
AnnaT7459 <141446349+AnnaT7459@users.noreply.github.com>
2024-03-11 17:13:07 -0500

    updated app, wrapped header and footer within apollo provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,13 @@
 import { Outlet } from 'react-router-dom'
 // import { Provider } from 'react-redux'
 // import store from './utils/store'
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 // import css
 import './App.css'
@@ -11,8 +17,22 @@ import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
@@ -20,11 +40,11 @@ const App = () => {
 
   return (
     <>
-    <Header />
     <ApolloProvider client={client}>
+    <Header />
     <Outlet />
-    </ApolloProvider>
     <Footer />
+    </ApolloProvider>
     </>
   )
 }
