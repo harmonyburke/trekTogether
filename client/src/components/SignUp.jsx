5fea9597ce31f593f950f8de470f800d2e7a7cfe
Harmony Burke <theharmonyburke@gmail.com>
2024-03-10 21:01:07 -0500

    set up signup.jsx for creating new users and added data to the db

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,6 +1,33 @@
 import randomImg from "../utils/randomImg";
 
-const Signup = () => {
+import { ADD_USER } from "../../utils/mutations";
+import { useMutation } from "@apollo/client";
+import { useState } from "react";
+
+const Signup = ( user) => {
+
+    const [addUser, {data:newUser}]= useMutation(ADD_USER);
+
+    const [email, setEmail]= useState(user.email);
+    const [name, setName]= useState(user.name);
+    const [password, setPassword] = useState(user.password);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try{
+            await addUser({
+                variables:{
+                    email, 
+                    name,
+                    password
+                }
+            })
+            console.log('New User added!')
+        }catch(err){
+            console.error('Error creating user.', err);
+        }
+    }
+
     return ( 
         <section>
             <form id="signUp-container" action="/signup" style={{ backgroundImage: `url(${randomImg()})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}>
@@ -9,17 +36,23 @@ const Signup = () => {
                  <input className='input'
                 type="text"
                 placeholder='Name'
+                defaultValue={name}
+                onChange={(e) => setName(e.target.value)}
                 required /> <br />
                 <input className='input'
                 type="email" 
                 placeholder='Email'
+                defaultValue={email}
+                onChange={(e) => setEmail(e.target.value)}
                 required/> <br />
                 {/* Password Input */}
                 <input className='input'
                 type="password"
                 placeholder='Password'
+                defaultValue={password}
+                onChange={(e) => setPassword(e.target.value)}
                 required /> <br />
-                <button type='submit' id='login-signup-btn'>Sign Up</button>
+                <button type='submit' id='login-signup-btn' onChange={handleSubmit}>Sign Up</button>
                 <br /><br /><br /><br /><br />
             </form>
         </section>
